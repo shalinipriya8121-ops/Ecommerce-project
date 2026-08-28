32785a802da920c9ffd87c5565d8740b5f8078da
Shalini Priya <shalinipriya8121@gmail.com>
2026-08-28 22:05:25 +0530

    Add my code

--- Project.cpp
+++ Project.cpp
@@ -1,58 +1,63 @@
 #include <iostream>
 using namespace std;
-enum Category{
+
+enum Category {
     Food,
     Books,
     Cloths,
     Electronics,
     Furniture
 };
-class Product{
+
+class Product {
     int Productid;
     string Name;
     int Price;
     int stock;
     Category category;
-    
+
 public:
-    
-        Product(int id,string n,int  p,int  s,Category c){
-        
-        Productid=id;
-         Name=n;
-        Price=p;
-        stock=s;
-        category=c;
-}
-    
 
-
-
-void displayProduct(){
-    cout<<"the productid is: "<<Productid<<endl;;
-    cout<<"product name is: "<<Name<<endl;
-    cout<<"product price is: "<<Price<<endl;
-    cout<<"stock: "<<stock<<endl;
-    cout<<"category: "<<category<<endl;
-}
-
-void updatestock(int quantity){
-    stock=stock+quantity;
-    cout<<"\nthe stock is updated successfully";
-}
-void checkAvailability(){
-    if(stock>0){
-        cout<<endl<<"it is available";
+    Product(int id, string n, int p, int s, Category c) {
+        Productid = id;
+        Name = n;
+        Price = p;
+        stock = s;
+        category = c;
     }
-    else{
-        cout<<"not available";
+
+    void displayProduct() {
+        cout << "The productid is: " << Productid << endl;
+        cout << "Product name is: " << Name << endl;
+        cout << "Product price is: " << Price << endl;
+        cout << "Stock: " << stock << endl;
+        cout << "Category: " << category << endl;
     }
-}};
-int main(){
-    Product p1(345,"Smart Phone",90000,19,Electronics);
+
+    void updatestock(int quantity) {
+        stock = stock + quantity;
+        cout << "The stock is updated successfully" << endl;
+    }
+
+    void checkAvailability() {
+        if (stock > 0) {
+            cout << "It is available" << endl;
+        }
+        else {
+            cout << "Not available" << endl;
+        }
+    }
+};
+
+int main() {
+
+    Product p1(345, "Smart Phone", 90000, 19, Electronics);
+
     p1.displayProduct();
-    p1.updatestock(10);
-    p1.checkAvailability();
-    
-}
 
+    p1.updatestock(10);
+
+    p1.checkAvailability();
+
+    return 0;
+}
